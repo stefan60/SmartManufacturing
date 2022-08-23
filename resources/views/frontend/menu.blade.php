68e88fa19ddf2ed3a83b4a0a633f8130ac745cf0
stefan60 <stefan60@dir.bg>
2022-08-23 16:36:27 +0300

    changes 01

---
+++ resources/views/frontend/menu.blade.php
@@ -0,0 +1,44 @@
+<link rel="stylesheet" href="/css/menu.css">
+
+<div class="sidebar">
+    <img src="/images/logo.jpg" alt="Фокс Лазер" style="width: 150px; height: 45px; padding: 10px 50px;" /><!-- &lt;Фокс Лазер&gt;-->
+    <hr class="hr">
+    <nav class="flex-column mainmenu">
+        <ul style="list-style-type: none">
+            <li class="nav-item px-4">
+                <span class="fa-solid fa-gauge-high inline" aria-hidden="true"></span>
+                <a href="">Показатели</a>
+            </li>
+            <li class="nav-item px-4">
+                <span span class="fa-solid fa-rectangle-list inline" aria-hidden="true"></span>
+                <a href="">Качество</a>
+            </li>
+            <li class="nav-item px-4">
+                <span class="fa-solid fa-chart-gantt inline" aria-hidden="true"></span>
+                <a href="">Планиране</a>
+            </li>
+            <li class="nav-item px-4">
+                <span class="fa-solid fa-gears inline" aria-hidden="true"></span>
+                <a href="">Операции</a>
+            </li>
+            <li class="nav-item px-4">
+                <span class="fa-solid fa-align-justify inline" aria-hidden="true"></span>
+                <a href="">Материали</a>
+            </li>
+            <li class="nav-item px-4">
+                <span class="fa-solid fa-screwdriver-wrench inline" aria-hidden="true"></span>
+                <a href="">Настройки</a>
+                <ul style="list-style-type: none; padding-left: 20px;" class="submenu">
+                    <li class="nav-item px-4">
+                        <span class="oi oi-caret-right inline" aria-hidden="true"></span>
+                        <a href="">База данни</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item pt-xxl-3">
+                <span class="fa-regular fa-circle-question inline" aria-hidden="true"></span>
+                <a href="">Помощ...</a>
+            </li>
+        </ul>
+    </nav>
+</div>
